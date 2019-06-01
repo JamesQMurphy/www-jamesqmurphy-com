10d35bd46a7cf5a14c57a5aaa166477847d7d00e
James Murphy <jamesqmurphy@yahoo.com>
2019-06-01 11:12:34 -0400

    Added ability to run ASP.NET web application as an AWS Lambda function (#4)

---
+++ src/JamesQMurphy.Web/LambdaEntryPoint.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Hosting;
+
+namespace JamesQMurphy.Web
+{
+    public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseStartup<Startup>();
+        }
+    }
+}
