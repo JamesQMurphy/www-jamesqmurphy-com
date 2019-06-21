5da2d6d44e09220e9d68b5ab3cd9e22730585425
James Murphy <jamurphy@arifleet.com>
2019-06-21 17:07:34 -0400

    think I've figured it out

--- build/validate-build-number.ps1
+++ build/validate-build-number.ps1
@@ -11,7 +11,21 @@ Write-Output "This branch version is $($env:VERSIONMAJOR).$($env:VERSIONMINOR).$
 $masterVersionMajor = -1
 $masterVersionMinor = -1
 $masterVersionPatch = -1
-& git --no-pager show --no-patch origin 'master:build/azure-pipelines.yml' | Where-Object { $_ -match '\s+version(Major|Minor|Patch):\s+(\d+)' } | ForEach-Object {
+
+<#
+When Azure DevOps clones our repo, it clones a shallow copy.  Then instead of checking out a branch, it does a
+checkout of the specific commit ID, like this:
+
+git checkout --progress --force <commit-id>
+
+This leaves the repo in 'Detached HEAD' state, which means that it's not on any branch.  Furthermore, since the
+clone is shallow, it doesn't have *any* knowledge of *any* of the branches (which is actually a good thing).  But
+since we need to compare against the version numbers in the master branch, we need to fetch that branch.
+#>
+& git fetch origin master
+
+# Now we've fetched the master branch, but git names the pointer FETCH_HEAD, not master
+& git --no-pager show FETCH_HEAD:build/azure-pipelines.yml | Where-Object { $_ -match '\s+version(Major|Minor|Patch):\s+(\d+)' } | ForEach-Object {
     switch($Matches[1]) {
         'Major' { $masterVersionMajor = $Matches[2] }
         'Minor' { $masterVersionMinor = $Matches[2] }
