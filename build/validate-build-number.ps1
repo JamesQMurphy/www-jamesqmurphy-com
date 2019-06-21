625000cec43bf83dfa3de8f809da563b5a3efffe
James Murphy <jamurphy@arifleet.com>
2019-06-21 14:45:19 -0400

    script name in quotes

--- build/validate-build-number.ps1
+++ build/validate-build-number.ps1
@@ -3,7 +3,7 @@
 # Read version number from build file
 
 
-. ./common-functions.ps1
+. './common-functions.ps1'
 
 Write-Output "This branch version is $($env:VERSIONMAJOR).$($env:VERSIONMINOR).$($env:VERSIONPATCH)"
 
