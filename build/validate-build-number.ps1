a1b1950d24b13bca5c424b2622924d4eb282817e
James Murphy <jamurphy@arifleet.com>
2019-06-21 16:24:41 -0400

    modified git show

--- build/validate-build-number.ps1
+++ build/validate-build-number.ps1
@@ -11,8 +11,7 @@ Write-Output "This branch version is $($env:VERSIONMAJOR).$($env:VERSIONMINOR).$
 $masterVersionMajor = -1
 $masterVersionMinor = -1
 $masterVersionPatch = -1
-& git fetch origin master
-& git --no-pager show master:build/azure-pipelines.yml | Where-Object { $_ -match '\s+version(Major|Minor|Patch):\s+(\d+)' } | ForEach-Object {
+& git --no-pager show --no-patch origin 'master:build/azure-pipelines.yml' | Where-Object { $_ -match '\s+version(Major|Minor|Patch):\s+(\d+)' } | ForEach-Object {
     switch($Matches[1]) {
         'Major' { $masterVersionMajor = $Matches[2] }
         'Minor' { $masterVersionMinor = $Matches[2] }
