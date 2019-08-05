c6a7c03952b0283d983f63747be1540d19963292
James Murphy <jamesqmurphy@yahoo.com>
2019-08-05 07:52:15 -0400

    Implemented build numbering scheme (#15)
    Build numbers are stored in `azure-pipelines.yml`.  These scripts were added to the build process:
    
     * validate-build-against-branch.ps1 - Compares against `master branch; fails the build if it is not up-to-date and does not have a higher version number
     * set-build-number.ps1 - Actuall sets the build number

---
+++ build/set-build-number.ps1
@@ -0,0 +1,69 @@
+<#
+
+Sets the build number based on the following rules, where N = 1, 2, 3
+
+1.  If the build is for the master branch, set it to Major.Minor.Patch[.N]
+2.  If the build is for a release branch, set it to Major.Minor.Patch-rc[.N]
+3.  If the build is for a pull request, set it to PR-xxxx[.N]
+4.  Otherwise, set it to Major.Minor.Patch-<branchname>[.N]
+
+See https://www.jamesqmurphy.com/blog/brewing-the-blog-6 for an explanation
+ 
+#>
+
+. "$PSScriptRoot/common-functions.ps1"
+
+$baseBuildNumber = switch -regex ($env:BUILD_SOURCEBRANCH) {
+    
+    'refs/heads/master'
+        { "$($env:VERSIONMAJOR).$($env:VERSIONMINOR).$($env:VERSIONPATCH)" }
+    
+    'refs/heads/releases/*'
+        { "$($env:VERSIONMAJOR).$($env:VERSIONMINOR).$($env:VERSIONPATCH)-rc" }
+
+    'refs/pull/(\d+)/merge'
+        { "PR-$($Matches[1])" }
+
+    default
+        { "$($env:VERSIONMAJOR).$($env:VERSIONMINOR).$($env:VERSIONPATCH)-$($env:BUILD_SOURCEBRANCHNAME)" }
+
+}
+
+Write-Output "Base build number: $baseBuildNumber"
+
+#Determine value of N to tack on the end
+$N = 0
+
+# Retrieve builds for this definition that match the pattern
+$previousBuildNumbers = Invoke-AzureDevOpsWebApi 'build/builds' -Version '5.0' -QueryString "definitions=$($env:SYSTEM_DEFINITIONID)&buildNumber=$baseBuildNumber*" | Select-Object -ExpandProperty Value | Select-Object -ExpandProperty buildNumber
+
+# Find the highest build number in the previous builds
+if (($previousBuildNumbers -ne $null) -and (@($previousBuildNumbers).Count -gt 0)) {
+    
+    Write-Output "Previous builds found that match $($baseBuildNumber): "
+    @($previousBuildNumbers) | ForEach-Object {
+        Write-Output " $_"
+    }
+
+    $N = 1
+    @($previousBuildNumbers) | Where-Object {$_ -like "$baseBuildNumber.*" } | ForEach-Object {
+        $split = $_ -split '\.'
+        $previousN = [Int32]::Parse($split[($split.Length - 1)])
+        if ($previousN -ge $N) {
+            $N = $previousN + 1
+        }
+    }
+}
+else {
+    Write-Output "No previous builds found beginning with $baseBuildNumber"
+}
+
+# Set actual build number        
+if ($N -eq 0) {
+    $newBuildNumber = $baseBuildNumber
+}
+else {
+    $newBuildNumber = "$baseBuildNumber.$N"
+}
+
+Write-AzureDevOpsLoggingCommand -Area build -Action updatebuildnumber -Message $newBuildNumber
