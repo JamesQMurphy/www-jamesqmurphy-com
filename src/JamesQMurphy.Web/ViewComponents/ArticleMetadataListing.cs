c27f3928083388c124984f73acb6c87c6d189be9
James Murphy <jamesqmurphy@yahoo.com>
2019-08-20 07:43:19 -0400

    Layout changes and some basic content (#26)
    * Added About view
    * Wrote basic story for About page
    * Created ArticleMetadataListing for viewing blog article headers
    * Put articles on front page
    * Removed cookie consent; updated Privacy Policy

---
+++ src/JamesQMurphy.Web/ViewComponents/ArticleMetadataListing.cs
@@ -0,0 +1,24 @@
+﻿using JamesQMurphy.Blog;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JamesQMurphy.Web.ViewComponents
+{
+    public class ArticleMetadataListing : ViewComponent
+    {
+        public IViewComponentResult Invoke(ArticleMetadata articleMetadata, bool displayCompact)
+        {
+            if (displayCompact)
+            {
+                return View("compact", articleMetadata);
+            }
+            else
+            {
+                return View(articleMetadata);
+            }
+        }
+    }
+}
