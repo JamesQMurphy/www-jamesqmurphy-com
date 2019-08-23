44c9e9681274f269a280b1f7cb6a35bd37963839
James Murphy <jamurphy@arifleet.com>
2019-08-23 13:42:32 -0400

    Added GetArticleTeaserMarkdown plus one test method

---
+++ src/JamesQMurphy.Web.UnitTests/HomePageItemsTests.cs
@@ -0,0 +1,32 @@
+﻿using JamesQMurphy.Blog;
+using JamesQMurphy.Web.Models;
+using NUnit.Framework;
+using System;
+
+namespace JamesQMurphy.Web.UnitTests
+{
+    public class HomePageItemsTests
+    {
+
+        private Article articleFromText(string text)
+        {
+            return new Article()
+            {
+                Content = text,
+                Slug = "some-slug",
+                Title = "Some Title",
+                PublishDate = DateTime.UtcNow
+            };
+        }
+
+        [Test]
+        public void TestBreakAtHeading()
+        {
+            var text = $"abcdef";
+            var content = $"{text}{Environment.NewLine}{Environment.NewLine}#Heading";
+
+            var teaser = HomePageItems.GetArticleTeaserMarkdown(articleFromText(content));
+            Assert.AreEqual($"{text}{Environment.NewLine}{Environment.NewLine}", teaser);
+        }
+    }
+}
--- src/JamesQMurphy.Web/Models/HomePageItems.cs
+++ src/JamesQMurphy.Web/Models/HomePageItems.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace JamesQMurphy.Web.Models
@@ -10,11 +11,28 @@ namespace JamesQMurphy.Web.Models
     {
         public readonly Article Article1 = null;
         public readonly Article Article2 = null;
+        private static string[] LineSplit = new string[1] { Environment.NewLine };
 
         public HomePageItems(Article article1, Article article2)
         {
             Article1 = article1;
             Article2 = article2;
+        }
+
+        public static string GetArticleTeaserMarkdown(Article article)
+        {
+            var sbTeaser = new StringBuilder();
+            var contentLines = article.Content.Split(LineSplit, StringSplitOptions.None);
+            foreach (var line in contentLines)
+            {
+                if (line.StartsWith("#"))
+                {
+                    break;
+                }
+                sbTeaser.AppendLine(line);
+            }
+
+            return sbTeaser.ToString();
         }
     }
 }
