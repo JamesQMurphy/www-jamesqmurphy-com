3a6bdb8b6ffcf6f38749925ac19849bc508fc3d8
James Murphy <jamurphy@arifleet.com>
2019-09-06 16:58:58 -0400

    Added new ApplicationUser class; work in progress

---
+++ src/JamesQMurphy.Web/Services/InMemoryUserStore.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using JamesQMurphy.Web.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace JamesQMurphy.Web.Services
+{
+    public class InMemoryUserStore : IUserStore<ApplicationUser>
+    {
+        private Dictionary<string, ApplicationUser> _dictUsers = new Dictionary<string, ApplicationUser>();
+
+        public Task<IdentityResult> CreateAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (_dictUsers.TryAdd(user.Id, user))
+            {
+                return Task.FromResult(IdentityResult.Success);
+            }
+            else
+            {
+                return Task.FromResult(IdentityResult.Failed(new IdentityError() { Description = "Already present" }));
+            }
+        }
+
+        public Task<IdentityResult> DeleteAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (_dictUsers.Remove(user.Id))
+            {
+                return Task.FromResult(IdentityResult.Success);
+            }
+            else
+            {
+                return Task.FromResult(IdentityResult.Failed(new IdentityError() { Description = "User not found" }));
+            }
+        }
+
+        public Task<ApplicationUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (_dictUsers.ContainsKey(userId))
+            {
+                return Task.FromResult(_dictUsers[userId]);
+            }
+            else
+            {
+                return Task.FromResult((ApplicationUser)null);
+            }
+        }
+
+        public Task<ApplicationUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.UserName.ToUpperInvariant());
+        }
+
+        public Task<string> GetUserIdAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(user.Id);
+        }
+
+        public Task<string> GetUserNameAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetNormalizedUserNameAsync(ApplicationUser user, string normalizedName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetUserNameAsync(ApplicationUser user, string userName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IdentityResult> UpdateAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        public void Dispose()
+        {
+            if (!disposedValue)
+            {
+                disposedValue = true;
+            }
+        }
+        #endregion
+    }
+}
