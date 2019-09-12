08c467d7dd65bf4a6219231024c858a14fb82143
James Murphy <JamesQMurphy@yahoo.com>
2019-09-11 23:35:11 -0400

    Removed unused using directive

--- src/JamesQMurphy.Web/Controllers/HomeController.cs
+++ src/JamesQMurphy.Web/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 using JamesQMurphy.Blog;
 using JamesQMurphy.Web.Models;
 
