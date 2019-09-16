478dfa7a9d86aa4d5e37ad12e70426aa590223b2
James Murphy <JamesQMurphy@yahoo.com>
2019-09-16 00:26:25 -0400

    Now using ServiceCollection instead of creating each individual object

--- src/JamesQMurphy.Web.UnitTests/ProfileControllerTests.cs
+++ src/JamesQMurphy.Web.UnitTests/ProfileControllerTests.cs
@@ -1,13 +1,12 @@
+using System;
 using JamesQMurphy.Email;
 using JamesQMurphy.Web.Controllers;
 using JamesQMurphy.Web.Models;
 using JamesQMurphy.Web.Models.ProfileViewModels;
 using JamesQMurphy.Web.Services;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using NUnit.Framework;
-using System;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Logging;
 
@@ -15,64 +14,34 @@ namespace JamesQMurphy.Web.UnitTests
 {
     public class ProfileControllerTests
     {
-        private ProfileController controller;
-        private ApplicationUserStore _userStore;
-        private ServiceCollection _serviceCollection = new ServiceCollection();
-        private PasswordHasher<ApplicationUser> _pwHasher = new PasswordHasher<ApplicationUser>();
-        private UpperInvariantLookupNormalizer _normalizer = new UpperInvariantLookupNormalizer();
+        private ProfileController _controller;
+        private ServiceProvider _serviceProvider; 
         private Microsoft.AspNetCore.Http.HttpContextAccessor _httpContextAccessor = new Microsoft.AspNetCore.Http.HttpContextAccessor();
 
         [SetUp]
         public void Setup()
         {
-            _serviceCollection.AddIdentity<ApplicationUser, ApplicationRole>()
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddIdentity<ApplicationUser, ApplicationRole>()
                 .AddDefaultTokenProviders()
                 .AddUserStore<ApplicationUserStore>()
                 .AddRoleStore<InMemoryRoleStore>()
                 .AddSignInManager<ApplicationSignInManager<ApplicationUser>>();
-            _serviceCollection.AddSingleton<ILoggerFactory, NullLoggerFactory>();
-            var serviceProvider = _serviceCollection.BuildServiceProvider();
+            serviceCollection.AddSingleton<ILoggerFactory, NullLoggerFactory>();
+            serviceCollection.AddSingleton<ILogger<UserManager<ApplicationUser>>>(NullLogger<UserManager<ApplicationUser>>.Instance);
+            serviceCollection.AddSingleton<ILogger<RoleManager<ApplicationRole>>>(NullLogger<RoleManager<ApplicationRole>>.Instance);
+            serviceCollection.AddSingleton<ILogger<SignInManager<ApplicationUser>>>(NullLogger<SignInManager<ApplicationUser>>.Instance);
+            serviceCollection.AddSingleton<IApplicationUserStorage, InMemoryApplicationUserStorage>();
+            serviceCollection.AddSingleton<IEmailService, NullEmailService>();
+            _serviceProvider = serviceCollection.BuildServiceProvider();
 
             _httpContextAccessor.HttpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
-            _httpContextAccessor.HttpContext.RequestServices = serviceProvider;
+            _httpContextAccessor.HttpContext.RequestServices = _serviceProvider;
 
-            _userStore = new ApplicationUserStore(new InMemoryApplicationUserStorage());
-
-            var identityOptions = new IdentityOptions();
-            var identityOptionsAccessor = ConfigurationHelper.OptionsFrom(identityOptions);
-
-            var userManager = new UserManager<ApplicationUser>(
-                _userStore,
-                identityOptionsAccessor,
-                _pwHasher,
-                null,
-                null,
-                _normalizer,
-                null,
-                serviceProvider,
-                NullLogger<UserManager<ApplicationUser>>.Instance);
-
-            var roleManager = new RoleManager<ApplicationRole>(
-                new InMemoryRoleStore(),
-                null,
-                _normalizer,
-                null,
-                NullLogger<RoleManager<ApplicationRole>>.Instance);
-
-            var userClaimsPrincipalFactory = new UserClaimsPrincipalFactory<ApplicationUser, ApplicationRole>(
-                userManager,
-                roleManager,
-                identityOptionsAccessor);
-
-            var signInManager = new ApplicationSignInManager<ApplicationUser>(
-                userManager,
-                _httpContextAccessor,
-                userClaimsPrincipalFactory,
-                identityOptionsAccessor,
-                NullLogger<SignInManager<ApplicationUser>>.Instance,
-                null);
-
-            controller = new ProfileController(signInManager, NullLogger<ProfileController>.Instance, new NullEmailService());
+            _controller = new ProfileController(
+                _serviceProvider.GetService<ApplicationSignInManager<ApplicationUser>>(),
+                NullLogger<ProfileController>.Instance,
+                _serviceProvider.GetService<IEmailService>());
         }
 
         [Test]
@@ -81,8 +50,8 @@ namespace JamesQMurphy.Web.UnitTests
             var email = "test@test";
             var username = "user1";
             var password = "abc123";
-            var user = AddExistingUser(email, password, username);
 
+            var user = AddExistingUser(_serviceProvider, email, password, username);
             var loginViewModel = new LoginViewModel()
             {
                 Email = email,
@@ -90,7 +59,7 @@ namespace JamesQMurphy.Web.UnitTests
                 RememberMe = false
             };
 
-            var result = controller.Login(loginViewModel).GetAwaiter().GetResult();
+            var result = _controller.Login(loginViewModel).GetAwaiter().GetResult();
 
             Assert.IsInstanceOf<Microsoft.AspNetCore.Mvc.RedirectToActionResult>(result);
             var redirectToActionResult = result as Microsoft.AspNetCore.Mvc.RedirectToActionResult;
@@ -98,17 +67,19 @@ namespace JamesQMurphy.Web.UnitTests
             Assert.AreEqual(nameof(HomeController.Index), redirectToActionResult.ActionName);
         }
 
-        private ApplicationUser AddExistingUser(string emailAddress, string password, string userName)
+        private static ApplicationUser AddExistingUser(IServiceProvider serviceProvider, string emailAddress, string password, string userName)
         {
+            var normalizer = serviceProvider.GetService<ILookupNormalizer>();
             var user = new ApplicationUser()
             {
                 Email = emailAddress,
-                NormalizedEmail = _normalizer.Normalize(emailAddress),
+                NormalizedEmail = normalizer.Normalize(emailAddress),
                 UserName = userName,
-                NormalizedUserName = _normalizer.Normalize(userName)
+                NormalizedUserName = normalizer.Normalize(userName)
             };
-            user.PasswordHash = _pwHasher.HashPassword(user, password);
-            _userStore.CreateAsync(user).GetAwaiter().GetResult();
+            user.PasswordHash = serviceProvider.GetService<IPasswordHasher<ApplicationUser>>().HashPassword(user, password);
+            var storage = serviceProvider.GetService<IApplicationUserStorage>();
+            storage.CreateAsync(user).GetAwaiter().GetResult();
             return user;
         }
 
