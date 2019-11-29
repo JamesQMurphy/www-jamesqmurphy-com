8507802a4ac342e42d77320d35feb6effb335ee7
James Murphy <jamesqmurphy@yahoo.com>
2019-11-29 01:31:20 -0500

    Used AWS SQS Queue for sending email (#53)

---
+++ src/JamesQMurphy.Lambda.EmailSender/LambdaEntryPoint.cs
@@ -0,0 +1,44 @@
+﻿using Amazon.Lambda.Core;
+using Amazon.Lambda.SQSEvents;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using JamesQMurphy.Email.Aws;
+using System;
+
+namespace JamesQMurphy.Lambda.EmailSender
+{
+    public static class LambdaEntryPoint
+    {
+        private static readonly string[] MessageSeparator = { Environment.NewLine };
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public static void Handler(SQSEvent sqsEvent, ILambdaContext context)
+        {
+            context.Logger.LogLine($"Received SQSEvent with {sqsEvent.Records.Count} SQSMessage record(s)");
+
+            context.Logger.LogLine("Creating EmailService object");
+            var emailService = new SESEmailService(new SESEmailService.Options()
+            {
+                FromAddress = "no-reply@jamesqmurphy.com"
+            });
+
+            foreach (var sqsMessage in sqsEvent.Records)
+            {
+                context.Logger.LogLine($"Processing message {sqsMessage.MessageId}");
+
+                // Parse out recipient, subject, and email body
+                var strArray = sqsMessage.Body.Split(MessageSeparator, 3, StringSplitOptions.None);
+                var recipient = strArray[0];
+                context.Logger.LogLine($"({sqsMessage.MessageId}) To: {recipient}");
+                var subject = strArray[1];
+                context.Logger.LogLine($"({sqsMessage.MessageId}) Subject: {subject}");
+                var body = strArray[2];
+                context.Logger.LogLine($"({sqsMessage.MessageId}) Subject: {body}");
+
+                // Send the e-mail
+                var result = emailService.SendEmailAsync(recipient, subject, body).GetAwaiter().GetResult();
+                context.Logger.LogLine($"({sqsMessage.MessageId}) Email service returned success: {result.Success} details: {result.Details}");
+            }
+        }
+    }
+}
