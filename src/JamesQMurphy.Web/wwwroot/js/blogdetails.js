33aae075449d1855080079a0c8e9750f6145d010
James Murphy <JamesQMurphy@yahoo.com>
2019-12-08 00:07:17 -0500

    Initial layout of comments

---
+++ src/JamesQMurphy.Web/wwwroot/js/blogdetails.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    getComments();
+});
+
+function getComments() {
+    $.getJSON(document.URL + '/comments', function (comments) {
+        $.each(comments, function (index, value) {
+            $("#commentsSection").append('<div class="media border mb-3"><p class="p-3"><b>' + value.authorName + '</b></p><div class="media-body"><p class="p-3">' + value.content + "</p></div></div>");
+        });
+    });
+}
