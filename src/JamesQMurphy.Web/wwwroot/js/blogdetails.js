a67d5ebede589e37ecabb761b703c1a3a28a503e
James Murphy <jamurphy@arifleet.com>
2019-12-13 12:45:06 -0500

    Made a "class" out of BlogComments

--- src/JamesQMurphy.Web/wwwroot/js/blogdetails.js
+++ src/JamesQMurphy.Web/wwwroot/js/blogdetails.js
@@ -1,26 +1,29 @@
 ﻿$(function () {
-    getLatestComments();
+    BlogComments.FetchLatestComments();
 });
 
-function getLatestComments() {
-    $.getJSON(document.URL + '/comments?sinceTimestamp=' + getLatestComments.lastTimestampRetrieved, function (commentsArray) {
+function BlogComments() { }
+
+
+BlogComments.lastTimestampRetrieved = '';
+
+BlogComments.FetchLatestComments = function () {
+    $.getJSON(document.URL + '/comments?sinceTimestamp=' + BlogComments.lastTimestampRetrieved, function (commentsArray) {
         if (commentsArray.length > 0) {
-            insertCommentsIntoDOM(commentsArray);
-            getLatestComments.lastTimestampRetrieved = commentsArray[commentsArray.length - 1].timestamp;
-            setTimeout(getLatestComments, 15000);
+            BlogComments.InsertCommentsIntoDOM(commentsArray);
+            BlogComments.lastTimestampRetrieved = commentsArray[commentsArray.length - 1].timestamp;
+            setTimeout(BlogComments.FetchLatestComments, 15000);
         }
         else {
-            setTimeout(getLatestComments, 15000);
+            setTimeout(BlogComments.FetchLatestComments, 15000);
         }
     });
-}
+};
 
-getLatestComments.lastTimestampRetrieved = '';
-
-function insertCommentsIntoDOM(commentsArray) {
+BlogComments.InsertCommentsIntoDOM = function (commentsArray) {
     $.each(commentsArray, function (index, blogArticleComment) {
         $("#commentsSection").append(
-            '<div class="jqm-comment media my-3" id="' + blogArticleComment.commentId + '" data-timestamp="' + blogArticleComment.timestamp +'">' +
+            '<div class="jqm-comment media my-3" id="' + blogArticleComment.commentId + '" data-timestamp="' + blogArticleComment.timestamp + '">' +
                 '<div class="jqm-comment-user-icon">' +
                     '<img class="img-fluid" src="' + blogArticleComment.authorImageUrl + '">' +
                 '</div>' +
@@ -30,4 +33,9 @@ function insertCommentsIntoDOM(commentsArray) {
                 '</div>' +
             '</div>');
     });
-}
+};
+
+
+
+
+
