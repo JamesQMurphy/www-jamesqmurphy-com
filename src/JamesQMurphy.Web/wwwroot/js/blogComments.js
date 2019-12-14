95c20ea5a192747c68f46095b1b4df959378bff4
James Murphy <JamesQMurphy@yahoo.com>
2019-12-14 10:46:21 -0500

    Added MutationObserver

--- src/JamesQMurphy.Web/wwwroot/js/blogComments.js
+++ src/JamesQMurphy.Web/wwwroot/js/blogComments.js
@@ -1,5 +1,10 @@
 ﻿$(function () {
-    $("#commentsSection").append(BlogComments.HtmlForMoreBlock('', 'SHOW MORE COMMENTS'));
+    var commentsSection = $("#commentsSection");
+
+    var mutationObserver = new MutationObserver(BlogComments.OnDOMChange);
+    mutationObserver.observe(commentsSection.get(0), { attributes: false, childList: true, characterData: false });
+
+    commentsSection.append(BlogComments.HtmlForMoreBlock('', 'SHOW MORE COMMENTS'));
     BlogComments.FetchLatestComments();
 });
 
@@ -22,42 +27,50 @@ BlogComments.FetchLatestComments = function () {
 };
 
 BlogComments.InsertCommentsIntoDOM = function (commentsArray) {
-    console.log("inside BlogComments.InsertCommentsIntoDOM, commentsArray = " + commentsArray.length);
-    $.each(commentsArray, function (index, blogArticleComment) {
+    console.log("inside BlogComments.InsertCommentsIntoDOM, commentsArray.length = " + commentsArray.length);
+    $.each(commentsArray, function (_index, blogArticleComment) {
+
+        // Figure out where to insert the new comment
         var parentId = blogArticleComment.replyToId;
         if (parentId.length === 0) {
             parentId = "commentsSection";
         }
+        var insertBeforeElement = $("#" + parentId + " .media[id][data-timestamp]").filter(function () {
+            return ($(this).data('timestamp') > blogArticleComment.timestamp)
+                && ($(this).attr('id').startsWith(blogArticleComment.replyToId + '/'));
+        }).first();
 
-        console.log("Looking under ID " + parentId);
-        console.log("replyToId is " + blogArticleComment.replyToId);
-        var insertBeforeElement = $("#" + parentId + " .media[id][data-timestamp]")
-            .filter(function () {
-                console.log('timestamp is ' + $(this).data('timestamp'));
-                console.log('id is ' + $(this).attr('id'));
-                return ($(this).data('timestamp') > blogArticleComment.timestamp)
-                    && ($(this).attr('id').startsWith(blogArticleComment.replyToId + '/'));
-            })
-            .first();
-
-        console.log(insertBeforeElement.length);
-
-        $('<div class="jqm-comment media my-3" id="' + blogArticleComment.commentId + '" data-timestamp="' + blogArticleComment.timestamp + '">' +
+        // Insert it
+        var newNode = $('<div class="jqm-comment media my-3" id="' + blogArticleComment.commentId + '" data-timestamp="' + blogArticleComment.timestamp + '">' +
             '<div class="jqm-comment-user-icon">' +
                 '<img class="img-fluid" src="' + blogArticleComment.authorImageUrl + '">' +
             '</div>' +
             '<div class="jqm-comment media-body px-3">' +
                 '<b>' + blogArticleComment.authorName + '</b> ' + (blogArticleComment.isMine ? '(you)' : '') + '<br/>' +
                 blogArticleComment.htmlContent +
-            BlogComments.HtmlForMoreBlock(blogArticleComment.commentId, 'VIEW REPLIES') +
+                BlogComments.HtmlForMoreBlock(blogArticleComment.commentId, 'VIEW REPLIES') +
             '</div>' + 
-          '</div>').insertBefore(insertBeforeElement);
+            '</div>').insertBefore(insertBeforeElement);
+
+        // Set up mutation observer under new node's media-body
+        var newNodeMediaBody = $(newNode).children('.media-body').get(0);
+        var mutationObserver = new MutationObserver(BlogComments.OnDOMChange);
+        mutationObserver.observe(newNodeMediaBody, { attributes: false, childList: true, characterData: false });
     });
 };
 
 BlogComments.HtmlForMoreBlock = function (id, viewText) {
     return '<div class="media my-3" id="' + id + '/more" data-timestamp="more">' +
         '<span id="' + id + '/view">' + viewText + '</span>&nbsp;</div>';
+};
+
+BlogComments.OnDOMChange = function (mutations) {
+    $.each(mutations, function (_index, mutationRecord) {
+        console.log("Mutation target: " + mutationRecord.target.getAttribute('id'));
+        $(mutationRecord.addedNodes).each(function (_index, addedNode) {
+            console.log("Added node: " + addedNode.getAttribute('id'));
+        });
+    });
 };
 
 // Polyfill for String.startsWith
