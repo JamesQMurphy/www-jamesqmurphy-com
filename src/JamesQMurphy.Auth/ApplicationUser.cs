b80220d6e62e87a00e7881fbd97a98a65de255d1
James Murphy <jamurphy@arifleet.com>
2019-12-31 15:31:38 -0500

    Started to defined ApplicationUserRecord

--- src/JamesQMurphy.Auth/ApplicationUser.cs
+++ src/JamesQMurphy.Auth/ApplicationUser.cs
@@ -1,10 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JamesQMurphy.Auth
 {
     public class ApplicationUser
     {
-        public string UserId { get; set; } = NewMiniGuid();
+        private const string RECORD_TYPE_ID = "ID";
+        private readonly Dictionary<string, ApplicationUserRecord> records = new Dictionary<string, ApplicationUserRecord>();
+
+        public ApplicationUser()
+        {
+            // Create a single ApplicationUserRecord to represent the ID
+            var miniGuid = NewMiniGuid();
+            records.Add(RECORD_TYPE_ID, new ApplicationUserRecord(RECORD_TYPE_ID, miniGuid, miniGuid, miniGuid));
+        }
+
+        public ApplicationUser(IEnumerable<ApplicationUserRecord> applicationUserRecords)
+        {
+            foreach(var rec in applicationUserRecords)
+            {
+                records.Add(rec.Provider, rec);
+            }
+        }
+
+        public string UserId => records[RECORD_TYPE_ID].Key;
+
+        ////////////////////////////////////////////////////////
+
+
         public string NormalizedEmail { get; set; }
         public string Email { get; set; }
         public bool EmailConfirmed { get; set; } = false;
---
+++ src/JamesQMurphy.Auth/ApplicationUserRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JamesQMurphy.Auth
+{
+    public class ApplicationUserRecord
+    {
+        public string Provider { get; set; }
+        public string Key { get; set; }
+        public string NormalizedKey { get; set; }
+        public string UserId { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public readonly Dictionary<string, string> StringAttributes = new Dictionary<string, string>();
+        public readonly Dictionary<string, bool> BoolAttributes = new Dictionary<string, bool>();
+
+        public ApplicationUserRecord(string provider, string key, string normalizedKey, string userId)
+        {
+            Provider = provider;
+            Key = key;
+            NormalizedKey = normalizedKey;
+            UserId = userId;
+        }
+    }
+}
