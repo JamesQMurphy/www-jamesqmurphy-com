57dc6256be272d693030b63b6749431f6c1e481e
James Murphy <JamesQMurphy@yahoo.com>
2020-01-18 11:08:53 -0500

    Added TitleHeading ViewComponent

---
+++ src/JamesQMurphy.Web/ViewComponents/TitleHeading.cs
@@ -0,0 +1,13 @@
+﻿using JamesQMurphy.Web.Models.ViewComponentModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JamesQMurphy.Web.ViewComponents
+{
+    public class TitleHeading : ViewComponent
+    {
+        public IViewComponentResult Invoke(string title)
+        {
+            return View(new TitleHeadingModel { Title = title });
+        }
+    }
+}
