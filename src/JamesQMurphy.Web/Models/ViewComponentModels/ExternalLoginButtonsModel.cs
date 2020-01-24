b7ba574dc7683d5b47b4650c1ab50065a7567b67
James Murphy <JamesQMurphy@yahoo.com>
2020-01-24 07:51:19 -0500

    Changed externalLoginButtons from partial view to view component

---
+++ src/JamesQMurphy.Web/Models/ViewComponentModels/ExternalLoginButtonsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JamesQMurphy.Web.Models.ViewComponentModels
+{
+    public class ExternalLoginButtonsModel
+    {
+        public string Caption { get; set; }
+    }
+}
