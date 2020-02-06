5977acfc0e8705bc4640f8c0cdbbc50f7f1ee1d0
James Murphy <JamesQMurphy@yahoo.com>
2020-02-06 07:45:49 -0500

    Added controller and view for profile page

---
+++ src/JamesQMurphy.Web/Controllers/profileController.cs
@@ -0,0 +1,23 @@
+﻿using JamesQMurphy.Web.Models;
+using JamesQMurphy.Web.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace JamesQMurphy.Web.Controllers
+{
+    [Authorize]
+    public class profileController : JqmControllerBase
+    {
+        public profileController(WebSiteOptions webSiteOptions) : base(webSiteOptions)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult index()
+        {
+            ViewData[Constants.VIEWDATA_PAGETITLE] = "Profile";
+            return View();
+        }
+    }
+}
